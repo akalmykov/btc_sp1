05413767e8fef52d3739e7b160e9ffa6364b85f5
akalmykov <alexey.kalmykoff@gmail.com>
2024-05-02 03:48:15 +0300

    merkle root recalculation

--- script/src/esplora_api.rs
+++ script/src/esplora_api.rs
@@ -1,4 +1,5 @@
 use hex::prelude::*;
+use serde_json::Value;
 
 // https://blockstream.info/api/blocks/837003
 // https://github.com/Blockstream/esplora/blob/master/API.md
@@ -11,7 +12,19 @@ pub async fn get_block_header(block_hash: &str) -> Result<[u8; 80], Box<dyn std:
     .await?
     .text()
     .await?;
-    println!("{}", resp);
 
     Ok(<[u8; 80]>::from_hex(resp.as_str())?)
 }
+
+pub async fn get_block_tx(block_hash: &str) -> Result<Value, Box<dyn std::error::Error>> {
+    let resp = reqwest::get(format!(
+        "https://blockstream.info/api/block/{block_hash}/txids"
+    ))
+    .await?
+    .text()
+    .await?;
+    let j: Value = serde_json::from_str(&resp)?;
+    Ok(j)
+}
+
+// https://blockstream.info/api/block/00000000000000000002f7538b43249a3120f12c3aa4f14f433ce4a61d0b44e0/txs/
--- script/src/main.rs
+++ script/src/main.rs
@@ -1,89 +1,68 @@
+use bitcoin::{
+    hashes::{sha256, sha256d, Hash},
+    merkle_tree, Txid,
+};
 use hex::prelude::*;
 use sp1_sdk::{utils, ProverClient, SP1Stdin};
-use std::error::Error;
+use std::{error::Error, str::FromStr};
 // use bitcoin::{block::Header, consensus::deserialize};
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 mod esplora_api;
-use esplora_api::get_block_header;
+use esplora_api::{get_block_header, get_block_tx};
 
+// https://people.csail.mit.edu/silvio/Selected%20Scientific%20Papers/Zero%20Knowledge/Zero-Knowledge_Sets.pdf
+//https://alinush.github.io/2023/02/05/Why-you-should-probably-never-sort-your-Merkle-trees-leaves.html
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let raw_header =
-        get_block_header("00000000000000000002f7538b43249a3120f12c3aa4f14f433ce4a61d0b44e0")
-            .await?;
-    // let some_header: Header = deserialize(&header_hex)?;
-    // let s = "006000208b91a6f87fb412bbdf95b67f944fca42313eaab55c370100000000000000000004efe0609fd39b308e80975743f32dd023ca7a1117d0e744b629412b0046c23e9f650866d36203177d0fa204";
-    // let n = <[u8; 80]>::from_hex(s).unwrap();
-    utils::setup_logger();
-    // Generate proof.
-    let mut stdin = SP1Stdin::new();
-    // let n = 186u32;
-    // stdin.write(&n);
-    let mut stdin = SP1Stdin::new();
-    stdin.write_slice(&raw_header);
+    let bloch_hash = "00000000000000000002f7538b43249a3120f12c3aa4f14f433ce4a61d0b44e0";
+    let raw_header = get_block_header(&bloch_hash).await?;
 
-    let client = ProverClient::new();
-    let mut proof = client.prove(ELF, stdin).expect("proving failed");
+    let tx_json = get_block_tx(bloch_hash).await?;
+    let mut hash_vec = vec![];
+    for tx in tx_json.as_array().unwrap() {
+        let mut hash_bytes = <[u8; 32]>::from_hex(tx.as_str().unwrap())?;
+        hash_bytes.reverse();
+        let btc_hash = sha256d::Hash::from_slice(&hash_bytes)?;
+        hash_vec.push(btc_hash);
+    }
+    let root = merkle_tree::calculate_root(hash_vec.into_iter());
+    match root {
+        Some(root) => {
+            println!("calculated merkle root2: {:?}", root);
+        }
+        None => {
+            println!("could not calculate root");
+        }
+    }
 
-    // Read output.
-    let a = proof.public_values.read::<[u8; 32]>();
-    // let b = proof.public_values.read::<u128>();
-    let hex_string = a.to_lower_hex_string();
-    println!("a: {}", hex_string);
-    // println!("b: {}", b);
-
-    // Verify proof.
-    client.verify(ELF, &proof).expect("verification failed");
-
-    // Save proof.
-    proof
-        .save("proof-with-io.json")
-        .expect("saving proof failed");
-
-    println!("successfully generated and verified proof for the program!");
     Ok(())
+
+    // utils::setup_logger();
+    // // Generate proof.
+    // let mut stdin = SP1Stdin::new();
+    // // let n = 186u32;
+    // // stdin.write(&n);
+    // let mut stdin = SP1Stdin::new();
+    // stdin.write_slice(&raw_header);
+
+    // let client = ProverClient::new();
+    // let mut proof = client.prove(ELF, stdin).expect("proving failed");
+
+    // // Read output.
+    // let a = proof.public_values.read::<[u8; 32]>();
+    // // let b = proof.public_values.read::<u128>();
+    // let hex_string = a.to_lower_hex_string();
+    // println!("a: {}", hex_string);
+    // // println!("b: {}", b);
+
+    // // Verify proof.
+    // client.verify(ELF, &proof).expect("verification failed");
+
+    // // Save proof.
+    // proof
+    //     .save("proof-with-io.json")
+    //     .expect("saving proof failed");
+
+    // println!("successfully generated and verified proof for the program!");
+    // Ok(())
 }
-
-// //! A simple script to generate and verify the proof of a given program.
-
-// use hex::prelude::*;
-// use std::error::Error;
-
-// // use bitcoin::{block::Header, consensus::deserialize};
-// use sp1_sdk::{SP1Prover, SP1Stdin, SP1Verifier};
-// const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
-// mod esplora_api;
-// use esplora_api::get_block_header;
-
-// // #[tokio::main]
-// //async fn main() -> Result<(), Box<dyn Error>> {
-// fn main() {
-//     // let raw_header =
-//     //     get_block_header("00000000000000000002f7538b43249a3120f12c3aa4f14f433ce4a61d0b44e0")
-//     //         .await?;
-//     // let some_header: Header = deserialize(&header_hex)?;
-//     // let s = "006000208b91a6f87fb412bbdf95b67f944fca42313eaab55c370100000000000000000004efe0609fd39b308e80975743f32dd023ca7a1117d0e744b629412b0046c23e9f650866d36203177d0fa204";
-//     // let n = <[u8; 80]>::from_hex(s).unwrap();
-//     // // Generate proof.
-//     let mut stdin = SP1Stdin::new();
-//     let n = [1, 2, 3];
-//     stdin.write_slice(&n);
-//     let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
-
-//     // Read output.
-//     let a = proof.stdout.read::<u128>();
-//     let b = proof.stdout.read::<u128>();
-//     println!("a: {}", a);
-//     println!("b: {}", b);
-
-//     // Verify proof.
-//     SP1Verifier::verify(ELF, &proof).expect("verification failed");
-
-//     // Save proof.
-//     proof
-//         .save("proof-with-io.json")
-//         .expect("saving proof failed");
-
-//     println!("successfully generated and verified proof for the program!");
-//     // Ok(())
-// }
